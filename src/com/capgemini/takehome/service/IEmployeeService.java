f6c58f35ba0c13e0d12becedc1dba3a19b719f10
Mittal <raj.a.mittal@capgemini.com>
2019-07-23 09:09:34 +0530

    first commit

---
+++ src/com/capgemini/takehome/service/IEmployeeService.java
@@ -0,0 +1,28 @@
+package com.capgemini.takehome.service;
+
+import java.sql.SQLException;
+
+import com.capgemini.takehome.bean.Employee;
+
+
+public interface IEmployeeService {
+
+
+
+boolean namevalid(String quan);
+
+
+boolean setData(int code, String quan, int sal, String desgn) throws SQLException, ClassNotFoundException;
+
+
+boolean codevalid(int code);
+
+
+boolean desgnvalid(String quan);
+
+
+Employee getData(int id) throws SQLException, ClassNotFoundException;
+
+
+
+}
